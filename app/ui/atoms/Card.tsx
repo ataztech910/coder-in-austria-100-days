ab3461850b98ee5442b16c808fd999a8b29250e2
ataztech910 <webconsult.ekb@gmail.com>
2023-04-27 02:05:14 +0200

    add main content

--- app/ui/atoms/Card.tsx
+++ app/ui/atoms/Card.tsx
@@ -1,15 +1,14 @@
 import styles from '@/app/ui/atoms/card.module.scss';
-import Image from 'next/image';
 
-export default function Card(cardData: ICard) {
+export default function Card(cardData: any) {
     return(
         <div className={styles.card}>
             <picture>
-                <source media="(min-width: 1280px)" srcSet={cardData.imageUrl[0]} width='100%' />
-                <img loading="lazy" src={cardData.imageUrl[1]} width='100%' alt={cardData.title} />
+                <source media="(min-width: 1280px)" srcSet={`https:${cardData.cardImages[1].fields.file.url}`} width='100%' />
+                <img loading="lazy" src={`https:${cardData.cardImages[0].fields.file.url}`} width='100%' alt={cardData.title} />
             </picture>
             <div className={styles.card__title}>{cardData.title}</div>
-            <div className={styles.card__description}>{cardData.description}</div>
+            <div className={styles.card__description}>{cardData.description.content[0].content[0].value}</div>
         </div>
     );
 }
--- app/ui/molecules/Statistics.tsx
+++ app/ui/molecules/Statistics.tsx
@@ -1,21 +1,17 @@
 import styles from '@/app/ui/molecules/statistics.module.scss';
 import React from 'react';
 
-export default function Statistics({style}: {style: string}) {
+export default function Statistics(statisticsData: any) {
     return(
-        <div className={`${style} ${styles.statistics}`}>
-            <div className={styles.statistics__item}>
-                <div className={styles.statistics__item__number}>10k</div>
-                <div className={styles.statistics__item__description}>Our active users</div>
-            </div>
-            <div className={styles.statistics__item}>
-                <div className={styles.statistics__item__number}>1k</div>
-                <div className={styles.statistics__item__description}>Professional mentors</div>
-            </div>
-            <div className={styles.statistics__item}>
-                <div className={styles.statistics__item__number}>15</div>
-                <div className={styles.statistics__item__description}>Unique courses</div>
-            </div>
+        <div className={` ${styles.statistics}`}>
+            {
+                statisticsData?.statisticItems?.map((item: any) => 
+                        <div key={item.fields.numberAsString} className={styles.statistics__item}>
+                            <div className={styles.statistics__item__number}>{item.fields.numberAsString}</div>
+                            <div className={styles.statistics__item__description}>{item.fields.fieldDescription}</div>
+                        </div>
+                )
+            }
         </div>
     );
 }
