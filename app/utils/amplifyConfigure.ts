629a8695bf69f1c13111fa6c0b945daebb0455c7
ataztech910 <webconsult.ekb@gmail.com>
2023-06-12 00:49:36 +0200

    hotfix

--- app/utils/amplifyConfigure.ts
+++ app/utils/amplifyConfigure.ts
@@ -2,6 +2,7 @@
 
 import { Amplify, Auth, API } from "aws-amplify";
 import awsExports from "../aws-exports";
+import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure({ config: { awsExports } });
 Auth.configure({ ...awsExports, ssr: true });
